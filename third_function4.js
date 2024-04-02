24e470f753902893ae0f946d6bfa01f013fce9af
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2024-04-02 22:58:39 +0530

    Create third_function4.js
     third_function4.js adding

---
+++ third_function4.js
@@ -0,0 +1,10 @@
+//Problem 4: Write a function to check if the char is a small case or not.
+
+function checking_lowecase(char){
+  if(char===char.toLowerCase()){
+    console.log("Lower case")
+  }else{
+    console.log("Upper case")
+  }
+}
+checking_lowecase("a");
